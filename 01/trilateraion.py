29ba2b77df53ee4179f655875ed583415c7f542a
Kunal Pednekar <kpednekar95@gmail.com>
2017-10-16 10:53:09 -0400

    Updated rotation matrix
    We now get the correct rotation matrix because theta is now correct. Next steps are to solve for x and y

--- 01/trilateraion.py
+++ 01/trilateraion.py
@@ -2,25 +2,36 @@ import sys, math, numpy
 
 def trilaterate3D(distances):
     origin = [0.,0.,0.,0.]
-    #get the offset to move first point to origin
-    translate = [a_i - b_i for a_i, b_i in zip(origin, distances[0])]
-    #ensure that the distance never gets affected
-    translate[3] = 0
-    #translate all points, first point is on origin
+    # convert to numpy array for greater manipulation
+    coord = numpy.array(distances)
+    # keep a copy of distances
+    d = coord[:,3]
+    # delete distances from coord matrix
+    coord = numpy.delete(coord, 3, axis=1)
+    # get the offset to move first point to origin
+    translate = [a_i - b_i for a_i, b_i in zip(origin, coord[0])]
+    # translate all points, first point is on origin
     for i in range(4):
-        distances[i] = map(sum, zip(distances[i], translate))
-    #we want the rotation matrix (around z-axis) with angle pi + arc tan (y,x) for the second point
-    #this would ensure first point is origin and second point is on the x axis
-    theta = math.atan2(distances[1][1], distances[1][0]) + math.pi
+        coord[i] = map(sum, zip(coord[i], translate))
+    # we want the rotation matrix (around z-axis) with angle pi + arc tan (y,x) for the second point
+    # this would ensure first point is origin and second point is on the x axis
+    theta = 2*math.pi - math.atan2(coord[1][1], coord[1][0])
+    print
     print "theta is: ", theta
-    rotationZ = [[math.cos(theta), -math.sin(theta), 0, 0],
-                [math.sin(theta), math.cos(theta), 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1]]
-    print"rotation matrix:\n",(numpy.matrix(rotationZ))
-    #perform rotation
-    distances = numpy.dot(rotationZ, distances)
-    print "after rotation:\n",numpy.matrix(distances)
+    rotationZ = [[math.cos(theta), -math.sin(theta), 0],
+                [math.sin(theta), math.cos(theta), 0],
+                [0, 0, 1]]
+    print
+    print "rotation matrix:\n",(numpy.matrix(rotationZ))
+    # perform rotation
+    coord = numpy.transpose(coord)
+    print
+    print "rotated coordinates (columns are x,y,z): \n", numpy.matrix(coord)
+    # rotate the array
+    coord = numpy.dot(rotationZ, coord)
+    print
+    numpy.set_printoptions(suppress=True)
+    print "after rotation(column are x,y,z):\n",numpy.matrix(coord)
     return [0.,0.,0.,0.]
 
 if __name__ == "__main__":
